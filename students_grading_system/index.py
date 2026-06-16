d9b65de8dc98ad619d0e269486afec2237760a55
Israel-O-John <osuborisrael3@gmail.com>
2026-06-16 22:09:19 +0100

    added new functions. organised code based on user.

--- students_grading_system/index.py
+++ students_grading_system/index.py
@@ -3,7 +3,6 @@ import secrets
 from tabulate import tabulate
 from base_data import student_classes_data as classes_data
 
-
 student_classes = {}
 SCORE_LIMITS = {
     "test": 25,
@@ -32,12 +31,12 @@ def main():
     if menu == "admin":
         while True:
             task = (
-            input(
-                "What operation do you intend to render? Student Management | Teacher Management | Check database | Exit:\n"
+                input(
+                    "What operation do you intend to render? Student Management | Teacher Management | Check database | Exit:\n"
+                )
+                .lower()
+                .strip()
             )
-            .lower()
-            .strip()
-        )
             match task:
                 case "check database":
                     # table_formatted_database()
@@ -51,78 +50,101 @@ def main():
                 case _:
                     print(f"{task} operation not recognised")
 
-
     if menu == "teacher":
         while True:
             task = (
-            input(
-                "What operation do you intend to render? View Students | Edit scores | Exit:\n"
+                input(
+                    "What operation do you intend to render? View Students | Edit scores | Exit:\n"
+                )
+                .lower()
+                .strip()
             )
-            .lower()
-            .strip()
-        )
             match task:
                 case "view students":
                     view_students()
                 case "edit scores":
                     teacher_scores_entry()
+                case "exit":
+                    break
                 case _:
-                    print(f"{task} operation is either not on this system or not within your juristiction")
-
+                    print(
+                        f"{task} operation is either not on this system or not within your juristiction"
+                    )
 
     if menu == "student":
-        print(json.dumps(student_classes, indent=4))
-        get_report_card()
+        report = get_report_card()
+        print(report)
 
 
 def student_management_sys():
     while True:
-            task = (
+        task = (
             input(
                 "What operation do you intend to render? Add Student | Delete student | Update Student data | Exit:\n"
             )
             .lower()
             .strip()
         )
-            match task:
-                case "add student":
-                    add_student_to_class()
-                case "delete student":
-                    delete_student()
-                case "update student data":
-                    update_student_data()
-                case "exit":
-                    break
-                case _:
-                    print(f"{task} operation not recognised")
+        match task:
+            case "add student":
+                add_student_to_class()
+            case "delete student":
+                delete_student()
+            case "update student data":
+                update_student_data()
+            case "exit":
+                break
+            case _:
+                print(f"{task} operation not recognised")
 
 
 def teacher_management_sys():
     while True:
-            task = (
+        task = (
             input(
                 "What operation do you intend to render? Add Teacher | Delete Teacher | Update Teacher data | Exit:\n"
             )
             .lower()
             .strip()
         )
-            match task:
-                case "add teacher":
-                    create_teacher()
-                case "delete teacher":
-                    delete_teacher()
-                case "update teacher data":
-                    update_teacher_data()
-                case "exit":
-                    break
-                case _:
-                    print(f"{task} operation not recognised")
-    
-    
-    
-    
-    
-    
+        match task:
+            case "add teacher":
+                create_teacher()
+            case "delete teacher":
+                delete_teacher()
+            # case "update teacher data":
+            #     update_teacher_data()
+            case "exit":
+                break
+            case _:
+                print(f"{task} operation not recognised")
+
+    ##############################################################
+    ####################   FOR ALL   #############################
+    ##############################################################
+
+
+def find_student(student_id=None):
+    # this is, so the func can be called both with an ID passed as a parameter or not passed
+    if student_id is None:
+        student_id = input("Student ID: ").strip()
+    else:
+        student_id = str(student_id).strip()
+
+    class_name, class_details = find_class_by_condition(
+        lambda name, details: any(
+            s["student_id"] == student_id for s in details["students"]
+        )
+    )
+
+    if class_details:
+        for student in class_details["students"]:
+            if student["student_id"] == student_id:
+                return (student, class_name, class_details)
+
+    return None, None, None
+
+
 def base_file_config():
     # checking if file exists
     try:
@@ -143,6 +165,38 @@ def find_class_by_condition(condition_func):
         if condition_func(class_name, class_details):
             return class_name, class_details
     return None, None
+
+    ############################################################
+    ####################   FOR ADMIN   #########################
+    ############################################################
+
+
+# Asks for students basic info and assigns student an id
+def create_student_basic():
+    # generate random id
+    PREFIX = "STU-"
+    secure_gen = secrets.SystemRandom()
+    random_number = secure_gen.randint(10000, 100000)
+
+    # basic info to ask student to provide
+    basic_question = [
+        "Surname",
+        "First Name",
+        "Middle name",
+        "Date of Birth(YYYY-MM-DD)",
+        "Student class",
+        "Guardian name",
+        "Guardian mail",
+    ]
+    basic_info = {
+        info.lower(): input(f"What's your {info}? ").lower() for info in basic_question
+    }
+
+    # merges the basic info with additional key and value pair
+    basic_info |= {"student_id": f"{PREFIX}{random_number}"}
+
+    # returns a dict of student info
+    return basic_info
 
 
 # Creates a student and add student to specified class
@@ -190,7 +244,7 @@ def create_teacher():
     )
 
     if class_details:
-        class_details["teacher"] = teach_name
+        class_details["teacher"] = teach_name.capitalize()
         class_details["teacher_id"] = teach_id
         print(
             f"{teach_name.capitalize()} is assigned to class {class_name.capitalize()}"
@@ -201,11 +255,100 @@ def create_teacher():
 
 
 def delete_teacher():
-    print("Delete Teacher")
+    teacher_name = input("Teacher Name: ").strip().capitalize()
+    teacher_id = input("Teacher ID: ").strip()
+    class_name, class_details = find_class_by_condition(
+        lambda name, detials: detials["teacher_id"] == teacher_id
+        and detials["teacher"] == teacher_name
+    )
+
+    print(class_name, class_details)
+    if class_details:
+        class_details["teacher"] = ""
+        class_details["teacher_id"] = ""
+        save_to_file()
+        print(f"{teacher_name} with ID of {teacher_id} has been removed from system")
+    else:
+        print("Invalid input | Teacher not on the system.")
 
 
+"""
+# to be considered way later
 def update_teacher_data():
     print("Update Teacher Data")
+"""
+
+
+def delete_student(student_id=None):
+    if student_id is None:
+        student_id = input("Student ID: ").strip()
+    else:
+        student_id = str(student_id).strip()
+
+    student, _, _ = find_student(student_id)
+    if student is None:
+        print(f"Student with ID:{student_id} does not exist on the system.")
+        return
+
+    class_name, class_details = find_class_by_condition(
+        lambda name, details: student in details["students"]
+    )
+    if class_details:
+        class_details["students"].remove(student)
+        print(
+            f"Student with ID:{student_id} in {class_name.capitalize()} has been removed from the system."
+        )
+        save_to_file()
+        return
+
+
+def update_student_data():
+    student, class_name, class_details = find_student()
+    if student == None and class_name == None and class_details == None:
+        print("Student does not exist on the system")
+        return
+
+    changes = input("Edit Info | Change class \n").strip().lower()
+    if changes == "edit info":
+        change_type_sys = [
+            "guardian name",
+            "guardian mail",
+            "surname",
+            "first name",
+            "middle name",
+        ]
+        usr_change = input("what to change? \n").strip().lower()
+        if usr_change in change_type_sys:
+            change = input(f"Input {usr_change}: ").strip().lower()
+            student[usr_change] = change
+            save_to_file()
+            print(f"{usr_change} update has been effected.")
+        else:
+            print("Such operation not recognised!")
+    elif changes == "change class":
+        new_class = input("Transfer to Class: ").strip().lower()
+        new_class_name, new_class_details = find_class_by_condition(
+            lambda name, details: new_class == name
+        )
+        if new_class_name == None:
+            print("That class does not exist!")
+            return
+        else:
+            class_details["students"].remove(student)
+            student["student class"] = new_class_name
+            add_student_to_class(student)
+            save_to_file()
+    else:
+        print("ERROR: Choose operation to perform from the listed")
+
+
+def table_formatted_database():
+    return "Love"
+
+    ##############################################################
+    ####################   FOR TEACHER   #########################
+    ##############################################################
+
 
 def view_students():
     teacher_id = input("Input your ID: ")
@@ -221,20 +364,6 @@ def view_students():
         return
 
 
-def get_report_card():
-    student_class = input("Student class: ").lower()
-    student_id = input("Input ID: ")
-
-    _, class_details = find_class_by_condition(
-        lambda name, details: name == student_class
-        and student_id in details["report_cards"].keys()
-    )
-    if class_details:
-        print(class_details["report_cards"][student_id])
-    else:
-        print("ERROR: Input Valid Class and Student ID")
-
-
 # gets a student and updates the student scores
 def teacher_scores_entry():
     teacher_id = input("What's your ID? ").strip()
@@ -243,7 +372,6 @@ def teacher_scores_entry():
         lambda name, details: details["teacher_id"] == teacher_id
     )
 
-
     if class_details:
         student_id = input("Student ID: ").strip()
         student, _, _ = find_student(student_id)
@@ -251,7 +379,7 @@ def teacher_scores_entry():
         if student is not None:
             try:
                 student["subjects_details"] = score_entry(student["subjects_details"])
-                student["teachers_remark"] = input("Your remark on this student: ")
+                student["teacher_remark"] = input("Your remark on this student: ")
                 class_details["report_cards"][student_id] = generate_report_card(
                     student
                 )
@@ -264,36 +392,6 @@ def teacher_scores_entry():
         print(f"No class found registered under Teacher ID: {teacher_id}")
 
 
-# Asks for students basic info and assigns student an id
-def create_student_basic():
-    # generate random id
-    PREFIX = "STU-"
-    secure_gen = secrets.SystemRandom()
-    random_number = secure_gen.randint(10000, 100000)
-
-    # basic info to ask student to provide
-    basic_question = [
-        "Surname",
-        "First Name",
-        "Middle name",
-        "Date of Birth(YYYY-MM-DD)",
-        "Student class",
-        "Guardian name",
-        "Guardian mail",
-    ]
-    basic_info = {
-        info.lower(): input(f"What's your {info}? ").lower() for info in basic_question
-    }
-
-    # merges the basic info with additional key and value pair
-    basic_info |= {"student_id": f"{PREFIX}{random_number}"}
-
-    # returns a dict of student info
-    return basic_info
-
-    # create a student a
-
-
 def score_entry(subjects_details):
     for subject_name, assessments in subjects_details.items():
         print(f"\n Entering scores for {subject_name}")
@@ -302,12 +400,17 @@ def score_entry(subjects_details):
             if criteria == "total":
                 continue
 
+            if criteria == "grade":
+                continue
+
             while True:
                 raw_score = input(f"Enter {criteria} score: ")
                 try:
                     score = float(raw_score)
                     if score < 0 or score > SCORE_LIMITS[criteria]:
-                        print(f"{criteria} score is in range of 1 to {SCORE_LIMITS[criteria]}")
+                        print(
+                            f"{criteria} score is in range of 1 to {SCORE_LIMITS[criteria]}"
+                        )
                         continue
                     assessments[criteria] = score
                     break
@@ -348,27 +451,6 @@ def student_grade(score):
         return "F9"
 
 
-def find_student(student_id=None):
-    # this is, so the func can be called both with an ID passed as a parameter or not passed
-    if student_id is None:
-        student_id = input("Student ID: ").strip()
-    else:
-        student_id = str(student_id).strip()
-
-    class_name, class_details = find_class_by_condition(
-        lambda name, details: any(
-            s["student_id"] == student_id for s in details["students"]
-        )
-    )
-
-    if class_details:
-        for student in class_details["students"]:
-            if student["student_id"] == student_id:
-                return (student, class_name, class_details)
-
-    return None, None, None
-
-
 def view_student_details(student_id=None):
     if student_id is None:
         student_id = input("Student ID: ").strip()
@@ -392,76 +474,11 @@ Subjects: {student["subjects"]}
 """
 
 
-def delete_student(student_id=None):
-    if student_id is None:
-        student_id = input("Student ID: ").strip()
-    else:
-        student_id = str(student_id).strip()
-
-    student, _, _ = find_student(student_id)
-    if student is None:
-        print(f"Student with ID:{student_id} does not exist on the system.")
-        return
-
-    class_name, class_details = find_class_by_condition(
-        lambda name, details: student in details["students"]
-    )
-    if class_details:
-        class_details["students"].remove(student)
-        print(
-            f"Student with ID:{student_id} in {class_name.capitalize()} has been removed from the system."
-        )
-        save_to_file()
-        return
-
-
-def update_student_data():
-    student, class_name, class_details = find_student()
-    if student == None and class_name == None and class_details == None:
-        print("Student does not exist on the system")
-        return
-
-
-    changes = input("Edit Info | Change class \n").strip().lower()
-    if changes == "edit info":
-        change_type_sys = [
-            "guardian name",
-            "guardian mail",
-            "surname",
-            "first name",
-            "middle name",
-        ]
-        usr_change = input("what to change? \n").strip().lower()
-        if usr_change in change_type_sys:
-            change = input(f"Input {usr_change}: ").strip().lower()
-            student[usr_change] = change
-            save_to_file()
-            print(f"{usr_change} update has been effected.")
-        else:
-            print("Such operation not recognised!")
-    elif changes == "change class":
-        new_class = input("Transfer to Class: ").strip().lower()
-        new_class_name, new_class_details = find_class_by_condition(
-            lambda name, details: new_class == name
-        )
-        if new_class_name == None:
-            print("That class does not exist!")
-            return
-        else:
-            class_details["students"].remove(student)
-            student["student class"] = new_class_name
-            add_student_to_class(student)
-            save_to_file()
-    else:
-        print("ERROR: Choose operation to perform from the listed")
-
-
-def table_formatted_database():
-    return "Love"
-
 def generate_report_card(student):
     headers = ["Subject", "Test", "Assignments", "Projects", "Exam", "Total", "Grade"]
     table_data = []
+
+    print(student)
 
     for subject, details in student["subjects_details"].items():
         row = [
@@ -482,13 +499,31 @@ def generate_report_card(student):
             ADDRESS: PO BOX 63922
             
     NAME:  {student["surname"].capitalize()} {student["first name"].capitalize()} {student["middle name"].capitalize()}
-    CLASS: {student["student_class"].title()}
+    CLASS: {student["student class"].title()}
     ID:    {student["student_id"]}
     
 {score_table}
 
 ~~~~~ Teacher's remark: {student["teacher_remark"]} ~~~~~~
     """
+
+    ##############################################################
+    ####################   FOR STUDENT   #########################
+    ##############################################################
+
+
+def get_report_card():
+    student_class = input("Student class: ").lower()
+    student_id = input("Input ID: ")
+
+    _, class_details = find_class_by_condition(
+        lambda name, details: name == student_class
+        and student_id in details["report_cards"].keys()
+    )
+    if class_details:
+        return class_details["report_cards"][student_id]
+    else:
+        print("ERROR: Check Student Class and Id | Student's Report Card is not ready.")
 
 
 if __name__ == "__main__":
